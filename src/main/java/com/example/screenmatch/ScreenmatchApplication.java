d95963726dc42b94ab1df584706a86deb52fd102
thales <thalesquadros2@gmail.com>
2025-01-16 20:15:55 -0300

    class changes implementation for clean code practices

--- src/main/java/com/example/screenmatch/ScreenmatchApplication.java
+++ src/main/java/com/example/screenmatch/ScreenmatchApplication.java
@@ -1,16 +1,9 @@
 package com.example.screenmatch;
 
-import com.example.screenmatch.model.EpisodeData;
-import com.example.screenmatch.model.SeasonData;
-import com.example.screenmatch.model.SerieData;
-import com.example.screenmatch.service.ApiSerie;
-import com.example.screenmatch.service.DataConvert;
+import com.example.screenmatch.main.Main;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -21,29 +14,8 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		ApiSerie apiSerie = new ApiSerie();
-		String json = apiSerie.returnData("http://www.omdbapi.com/?t=gilmore+girls&Season=1&apikey=70066399");
-		System.out.println(json);
-
-		DataConvert converter = new DataConvert();
-		SerieData data = converter.getData(json, SerieData.class);
-
-		Integer totalSeasons = data.totalSeasons();
-
-		System.out.println(data);
-		json = apiSerie.returnData("http://www.omdbapi.com/?t=gilmore+girls&Season=1&episode=2&apikey=70066399");
-		EpisodeData episode = converter.getData(json, EpisodeData.class);
-		System.out.println(episode);
-		SeasonData season;
-		List<SeasonData> seasons = new ArrayList<>();
-
-		for (int index=1; index <= totalSeasons; index++) {
-			json = apiSerie.returnData(
-					"http://www.omdbapi.com/?t=gilmore+girls&Season="+index+"&apikey=70066399"
-			);
-			season = converter.getData(json, SeasonData.class);
-			seasons.add(season);
-		}
-		seasons.forEach(System.out::println);
+		Main main = new Main();
+		main.exhibitData();
 	}
+
 }
---
+++ src/main/java/com/example/screenmatch/main/Main.java
@@ -0,0 +1,40 @@
+package com.example.screenmatch.main;
+
+import com.example.screenmatch.model.SeasonData;
+import com.example.screenmatch.model.SerieData;
+import com.example.screenmatch.service.ApiSerie;
+import com.example.screenmatch.service.DataConvert;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+    private Scanner scan = new Scanner(System.in);
+    private final String BASEURL = "https://www.omdbapi.com/?t=";
+    private final String API_KEY = "&apikey=70066399";
+    private ApiSerie apiSerie = new ApiSerie();
+    private DataConvert converter = new DataConvert();
+
+    public void exhibitData() {
+        System.out.println("Type the serie's name");
+        String serieName = scan.nextLine();
+        String address = BASEURL + serieName.replace(" ", "+") + API_KEY;
+
+        String json = apiSerie.returnData(address);
+        var data = converter.getData(json, SerieData.class);
+        System.out.println(data);
+
+        SeasonData season;
+        List<SeasonData> seasons = new ArrayList<>();
+
+        for (int index = 1; index <= data.totalSeasons(); index++) {
+            address = BASEURL + serieName.replace(" ", "+") +"&season=" + index + API_KEY;
+            json = apiSerie.returnData(address);
+            season = converter.getData(json, SeasonData.class);
+            seasons.add(season);
+        }
+        seasons.forEach(System.out::println);
+    }
+}
